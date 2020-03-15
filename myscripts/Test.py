43a000f832c7cf6177a245a7edfd367ae5be768e
Digi2learn-Arch <46841166+Digi2learn-Arch@users.noreply.github.com>
2020-03-15 22:59:23 +0100

    4 files changed
    4 files changed

--- myscripts/Test.py
+++ myscripts/Test.py
@@ -6,6 +6,12 @@ y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
 slope, intercept, r, p, std_err = stats.linregress(x, y)
 
+print(slope)
+print(intercept)
+print(r)
+print(p)
+print(std_err)
+
 def myfunc(x):
   return slope * x + intercept
 
@@ -17,4 +23,4 @@ plt.show()
 
 print('End of the Script')
 
-
+print('Second End of the Script')
---
+++ myscripts/gen_data.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import scipy.stats as scp
+import numpy as np
+
+
+x = np.random.random(10)
+y = 1.6* x + np.random.random(10)
+
+print('x : ',  x)
+print('y : ',  y)
